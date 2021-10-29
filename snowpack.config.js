30c927b5fdd2ba0f27487acb6174400e0ee2980a
bearbearbear <bear-bear-bear@kakao.com>
2021-10-29 17:36:45 +0900

    feat: 사용하지 않는 기존 설정 삭제

--- snowpack.config.js
+++ snowpack.config.js
@@ -2,25 +2,4 @@
 // See all supported options: https://www.snowpack.dev/reference/configuration
 
 /** @type {import("snowpack").SnowpackUserConfig } */
-module.exports = {
-  alias: {
-    '@app': './src',
-    '@styles': './src/styles',
-    '@scripts': './src/scripts',
-  },
-  mount: {
-    /* ... */
-  },
-  plugins: [
-    /* ... */
-  ],
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
-};
+module.exports = {};
