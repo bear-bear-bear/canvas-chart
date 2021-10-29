f4c09073d35e49fab743adf1d721f149648bd92f
bearbearbear <bear-bear-bear@kakao.com>
2021-10-29 18:22:58 +0900

    feat: 스노우팩-ghpages 설정 롤백

--- snowpack.config.js
+++ snowpack.config.js
@@ -2,15 +2,4 @@
 // See all supported options: https://www.snowpack.dev/reference/configuration
 
 /** @type {import("snowpack").SnowpackUserConfig } */
-module.exports = {
-  mount: {
-    public: { url: '/', static: true },
-    src: { url: '/dist' },
-  },
-  buildOptions: {
-    // put the build files in /docs
-    out: 'docs',
-    // put the meta snowpack build files under snowpack instead of _snowpack since Github special-cases underscore prefixed folders
-    metaUrlPath: 'snowpack',
-  },
-};
+module.exports = {};
