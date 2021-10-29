a106a986c2470303e6262573dc5d9c62906a9af1
bearbearbear <bear-bear-bear@kakao.com>
2021-10-29 17:58:40 +0900

    feat: snowpack을 gh-pages로 배포하기 위한 기반 설정

--- snowpack.config.js
+++ snowpack.config.js
@@ -2,4 +2,11 @@
 // See all supported options: https://www.snowpack.dev/reference/configuration
 
 /** @type {import("snowpack").SnowpackUserConfig } */
-module.exports = {};
+module.exports = {
+  buildOptions: {
+    // put the build files in /docs
+    out: 'docs',
+    // put the meta snowpack build files under snowpack instead of _snowpack since Github special-cases underscore prefixed folders
+    metaUrlPath: 'snowpack',
+  },
+};
