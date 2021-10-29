61aa4f17c52f4cf960081232316f68d40c307c9c
bearbearbear <bear-bear-bear@kakao.com>
2021-10-29 18:36:05 +0900

    feat: gh-pages로 배포를 위한 기반 설정

--- snowpack.config.js
+++ snowpack.config.js
@@ -2,4 +2,9 @@
 // See all supported options: https://www.snowpack.dev/reference/configuration
 
 /** @type {import("snowpack").SnowpackUserConfig } */
-module.exports = {};
+module.exports = {
+  mount: {
+    src: '/',
+    public: '/',
+  },
+};
